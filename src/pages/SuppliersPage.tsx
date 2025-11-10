4de732296390eb11a6eb551cb54d52ce0170c6c0
zix <ccvegas@academia.usbbog.edu.co>
2025-11-10 15:38:17 -0500

    fix: Agregar type assertion explícito al array de columnas en SuppliersPage

--- src/pages/SuppliersPage.tsx
+++ src/pages/SuppliersPage.tsx
@@ -269,7 +269,7 @@ export default function SuppliersPage() {
                 </div>
               )
             }
-          ]}
+          ] as Column<Supplier>[]}
           data={suppliers}
         />
       </Card>
